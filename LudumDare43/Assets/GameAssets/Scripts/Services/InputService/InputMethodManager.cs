835ae2c60aa6401a223c74ed43196abccc3a2acf
Unknown <luke123t@gmail.com>
2018-12-01 18:32:12 -0400

    Add Input Detection and Switching

---
+++ LudumDare43/Assets/GameAssets/Scripts/Services/InputService/InputMethodManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+namespace DogHouse.Services
+{
+    /// <summary>
+    /// InputMethodManager is a component that controls
+    /// which method of input is used.
+    /// </summary>
+    public class InputMethodManager : MonoBehaviour 
+    {
+        #region Private Variables
+        [SerializeField]
+        private GameObject m_keyboardMouseInput;
+
+        [SerializeField]
+        private GameObject m_gamepadService;
+
+        private InputState m_state;
+        #endregion
+
+        #region Main Methods
+        private void OnEnable()
+        {
+            SwitchInputSystems();
+        }
+
+        private void Update()
+        {
+            SwitchInputSystems();
+        }
+        #endregion
+
+        #region Utility Methods
+        private void SwitchInputSystems()
+        {
+            bool connected = DetermineIfGamepadIsConnected();
+
+            if(connected && m_state == InputState.KEYBOARD)
+            {
+                SetState(InputState.CONTROLLER);
+            }
+
+            if(!connected && m_state == InputState.CONTROLLER)
+            {
+                SetState(InputState.KEYBOARD);
+            }
+        }
+
+        private bool DetermineIfGamepadIsConnected()
+        {
+            string[] deviceNames = Input.GetJoystickNames();
+            return deviceNames.Length > 0;
+        }
+
+        private void SetState(InputState state)
+        {
+            m_state = state;
+            if(m_state == InputState.CONTROLLER)
+            {
+                m_gamepadService.SetActive(true);
+                m_keyboardMouseInput.SetActive(false);
+                return;
+            }
+
+            m_gamepadService.SetActive(false);
+            m_keyboardMouseInput.SetActive(true);
+        }
+        #endregion
+    }
+
+    public enum InputState
+    {
+        KEYBOARD,
+        CONTROLLER
+    }
+}

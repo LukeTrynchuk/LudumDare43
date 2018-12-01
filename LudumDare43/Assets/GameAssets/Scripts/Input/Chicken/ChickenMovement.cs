727dbf1908a76dc7a15c6e847763806871077054
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-12-01 19:11:56 -0400

    change to char controller

--- LudumDare43/Assets/GameAssets/Scripts/Input/Chicken/ChickenMovement.cs
+++ LudumDare43/Assets/GameAssets/Scripts/Input/Chicken/ChickenMovement.cs
@@ -7,13 +7,17 @@ using DogHouse.Core.Services;
 public class ChickenMovement : MonoBehaviour
 {
     private ServiceReference<IInputService> inputService = new ServiceReference<IInputService>();
-    private Rigidbody myBody;
+    //private Rigidbody myBody;
     public float moveSpeed;
+    private CharacterController myBody;
+    private Vector3 moveDir;
+    public float gravity;
+    public float jumpForce;
     // Start is called before the first frame update
     private void OnEnable()
     {
         inputService.AddRegistrationHandle(RegisterIntput);
-        myBody = GetComponent<Rigidbody>();
+        myBody = GetComponent<CharacterController>();
     }
     private void OnDisable()
     {
@@ -30,24 +34,14 @@ public class ChickenMovement : MonoBehaviour
     }
     private void MoveChicken(Vector2 move)
     {
-        if (move.x > 0.5f || move.x < -0.5f)
+        moveDir = new Vector3(move.x, 0f, move.y);
+
+        moveDir.y = moveDir.y + (Physics.gravity.y * Time.deltaTime * gravity);
+        if()
         {
-            //transform.Translate(new Vector3(move.x * moveSpeed * Time.deltaTime, 0f, 0f));
-            myBody.velocity = new Vector3(move.x * moveSpeed, myBody.velocity.y, myBody.velocity.z);
+            moveDir.y = jumpForce;
         }
-        if (move.y > 0.5f || move.y < -0.5f)
-        {
-            //transform.Translate(new Vector3(0f, 0f, move.x * moveSpeed * Time.deltaTime));
-            myBody.velocity = new Vector3(myBody.velocity.x, myBody.velocity.y, move.y * moveSpeed);
-        }
-        if (move.x < 0.5f && move.x > -0.5f)
-        {
-            myBody.velocity = new Vector3(0f, 0f, myBody.velocity.z);
-        }
-        if (move.y < 0.5f && move.y > -0.5f)
-        {
-            myBody.velocity = new Vector3(myBody.velocity.x, 0f, 0f);
-        }
+        myBody.Move(moveDir * Time.deltaTime * moveSpeed);
     }
 
 }

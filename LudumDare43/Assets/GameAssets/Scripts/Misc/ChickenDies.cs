c04b697c4544043434bfd055113fa9a9959ba691
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-12-02 14:19:12 -0400

    Killing Chickens/Dying

---
+++ LudumDare43/Assets/GameAssets/Scripts/Misc/ChickenDies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DogHouse.Core.Services;
+using DogHouse.Services;
+
+public class ChickenDies : MonoBehaviour
+{
+    private ServiceReference<ISceneManager> sceneMan = new ServiceReference<ISceneManager>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (sceneMan.isRegistered())
+            {
+                sceneMan.Reference.LoadMainMenuScene();
+            }
+        }
+        else if(other.tag == "Chicken")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

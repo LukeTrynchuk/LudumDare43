8880656db20a8a5aa7f2c241006fde6c9b344173
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-12-02 16:16:05 -0400

    Turret logic

---
+++ LudumDare43/Assets/GameAssets/Scripts/Misc/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DogHouse.Core.Services;
+using DogHouse.Services;
+
+public class Projectile : MonoBehaviour
+{
+    private ServiceReference<ISceneManager> sceneMan = new ServiceReference<ISceneManager>();
+    float lifeCounter = 1f;
+
+    private void Update()
+    {
+        lifeCounter -= Time.deltaTime;
+        if(lifeCounter<=0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            if (sceneMan.isRegistered())
+            {
+                sceneMan.Reference.LoadMainMenuScene();
+            }
+        }
+        else if (other.tag == "Chicken")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ LudumDare43/Assets/GameAssets/Scripts/Misc/TurretFire.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFire : MonoBehaviour
+{
+    public GameObject projectile;
+    [SerializeField]
+    private float reloadTime;
+    [SerializeField]
+    private float bulletSpeed;
+    private float counter;
+    public GameObject barrel;
+    private bool fired;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fired = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!fired)
+        {
+            Debug.Log("Fire raycast");
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+            {
+                if (hit.transform.tag == "Chicken" || hit.transform.tag == "Player")
+                {
+                    Debug.Log("Hit Chicken");
+                    GameObject p = Instantiate(projectile, barrel.transform.position, Quaternion.identity);
+                    p.GetComponent<Rigidbody>().AddForce(p.transform.forward * bulletSpeed, ForceMode.Impulse);
+                    fired = true;
+                }
+            }
+        }
+        else
+        {
+            counter += Time.deltaTime;
+            if(counter >= reloadTime)
+            {
+                counter = 0;
+                fired = false;
+            }
+        }
+
+    }
+}
